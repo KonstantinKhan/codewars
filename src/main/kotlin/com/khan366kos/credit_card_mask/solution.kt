f50b48da6715ccc42db00b47ffc7151eeacf213e
KonstantinKhan <khan366kos@gmail.com>
2025-04-02 11:34:35 +0500

    added solutions of credit_card_mask

--- src/main/kotlin/com/khan366kos/credit_card_mask/solution.kt
+++ src/main/kotlin/com/khan366kos/credit_card_mask/solution.kt
@@ -1,2 +1,7 @@
 package com.khan366kos.credit_card_mask
 
+fun maskify(cc: String): String = cc.takeLast(4).padStart(cc.length, '#')
+
+fun main() {
+    println(maskify("Nananananananananananananananana Batman!"))
+}
--- src/test/kotlin/com/khan366kos/credit_card_mask/Tests.kt
+++ src/test/kotlin/com/khan366kos/credit_card_mask/Tests.kt
@@ -1,4 +1,14 @@
 package com.khan366kos.credit_card_mask
 
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
 class Tests {
+    @Test
+    fun success() {
+        assertEquals(
+            "####################################man!",
+            maskify("Nananananananananananananananana Batman!")
+        )
+    }
 }
