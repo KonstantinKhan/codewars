f455ba8e93e13a64207125199bb6052fbb38609a
KonstantinKhan <khan366kos@gmail.com>
2025-04-02 09:42:37 +0500

    renameв test class

--- src/test/kotlin/com/khan366kos/disemvowel_trolls/Tests.kt
+++ src/test/kotlin/com/khan366kos/disemvowel_trolls/Tests.kt
@@ -3,7 +3,7 @@ package com.khan366kos.disemvowel_trolls
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-class Test {
+class Tests {
     @Test
     fun success(): Unit {
         assertEquals(disemvowel("This website is for losers LOL!"), "Ths wbst s fr lsrs LL!")
